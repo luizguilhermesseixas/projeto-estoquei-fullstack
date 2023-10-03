2275dffb347f73d787b6c0dabe14fa473e0524c2
Luiz Guilherme S Seixas <guilhermesschiavon@gmail.com>
2023-10-03 19:52:53 -0300

    Altera script no package para utilizar ts-node-dev e Inicia prisma

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import express, {Express, Request, Response} from 'express';
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.send({ message: 'HELLLOOOO!!' });
+});
 
 export default app;
--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -6,8 +6,4 @@ const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
 
-app.get('/', (req, res) => {
-  return res.send({message: 'Hello World!'});
-});
-
 export default server;
